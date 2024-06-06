a564a178221b52296b8e6cd5bc7d13c6990fc6a3
Lautaro-Guevara <lfguevara2001@gmail.com>
2024-06-06 19:04:01 -0300

    v1.0

---
+++ scripts/recipe-instructions.js
@@ -0,0 +1,25 @@
+import recipes from "./recipes.js";
+
+//Create Recipe instructions
+const selectedRecipeName = localStorage.getItem("selectedRecipe");
+const recipe = recipes.find(r => r.recipeName === selectedRecipeName);
+
+if (recipe){
+    document.getElementById("recipe-title").textContent = recipe.recipeName;
+
+    const recipeDetails = document.getElementById("recipe-details");
+    recipeDetails.innerHTML = `
+    <p>${recipe.description}</p>
+        <h3>Ingredients</h3>
+        <ul>${recipe.ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}</ul>
+        <h3>Instructions</h3>
+        <ol>${recipe.instructions.map(instruction => `<li>${instruction}</li>`).join('')}</ol>
+        <div>
+        <img src="${recipe.image}" alt="${recipe.recipeName}">
+        </div>
+    `;
+} else {
+    document.getElementById("recipe-title").textContent = "Recipe not found"
+    document.getElementById("recipe-details").innerHTML = '<p>Sorry, we couldn\'t find the recipe you\'re looking for.</p>';
+}
+
---
+++ scripts/script.js
@@ -0,0 +1,98 @@
+import { recipes } from "./recipes.js";
+
+// Función para seleccionar la receta del día
+function selectRecipeOfTheDay() {
+    let recipeOfTheDayIndex = localStorage.getItem("recipeOfTheDayIndex");
+    const today = new Date().toISOString().split('T')[0]; // Fecha actual en formato YYYY-MM-DD
+    const lastUpdate = localStorage.getItem("lastRecipeUpdate");
+
+    // Si la receta del día no se ha actualizado hoy, selecciona una nueva
+    if (lastUpdate !== today) {
+        recipeOfTheDayIndex = Math.floor(Math.random() * recipes.length);
+        localStorage.setItem("recipeOfTheDayIndex", recipeOfTheDayIndex);
+        localStorage.setItem("lastRecipeUpdate", today);
+    }
+console.log("Hoy es", today)
+
+    const recipeOfTheDay = recipes[recipeOfTheDayIndex];
+    const recipeElement = document.getElementById("recipe-of-the-day");
+    recipeElement.innerHTML = `
+        <h2>Recipe of the Day</h2>
+        <div class="recipe-of-the-day-info">
+            <img src="${recipeOfTheDay.image}" alt="${recipeOfTheDay.recipeName}">
+            <div class="recipe-of-the-day-text">
+                <a><h3>${recipeOfTheDay.recipeName}</h3></a>
+                <p>${recipeOfTheDay.description}</p>
+            </div>
+        </div>
+    `;
+}
+
+// carousel scripts
+console.log("Hola2")
+let left = document.getElementById("left");
+let right = document.getElementById("right");
+let images = document.getElementsByClassName("image")[0];
+let dots = document.getElementsByClassName("dots")[0];
+let text = document.getElementsByClassName("carousel-text")[0];
+let current = 0;
+carouselPosition();
+
+left.addEventListener("click", function(){
+    current -=1;
+
+    if(current == -1){
+        current = recipes.length - 1;
+    }
+
+    images.innerHTML = `
+    <img class="image" src="${recipes[current].image}" alt="page logo" loading="lazy"></img>
+    `;
+    text.innerHTML = `
+    <h3>${recipes[current].recipeName}</h3>
+    `;
+    carouselPosition();
+});
+
+right.addEventListener("click", function(){
+    current +=1;
+
+    if (current == recipes.length){
+        current = 0;
+    }
+
+    images.innerHTML = `
+    <img class="image" src="${recipes[current].image}" alt="page logo" loading="lazy"></img>
+    `;
+
+    text.innerHTML = `
+    <h3>${recipes[current].recipeName}</h3>
+    `;
+    carouselPosition();
+});
+
+function carouselPosition() {
+    dots.innerHTML = "";
+    for (var i = 0; i < recipes.length; i++) {
+        if (i == current) {
+            dots.innerHTML += '<p class="bold">.</p>';
+        } else {
+            dots.innerHTML += "<p>.</p>";
+        }
+    }
+};
+
+// Eventos DOMContentLoaded
+document.addEventListener("DOMContentLoaded", () => {
+    selectRecipeOfTheDay();
+
+    images.innerHTML = `
+    <img class="image" src="${recipes[current].image}" alt="page logo" loading="lazy"></img>
+    `;
+
+    text.innerHTML = `
+    <h3>${recipes[current].recipeName}</h3>
+    `;
+});
+
+console.log("Hola3")
